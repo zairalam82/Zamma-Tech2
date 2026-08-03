95fba5de9db94787cb67647ce62d826e4d3c940a
zairalam82 <zairalam82@gmail.com>
2026-08-04 01:22:53 +0500

    Update sequelize.js

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,13 +1,14 @@
-const {Sequelize}=require("sequelize");
+const { Sequelize } = require("sequelize");
 
-const sequelize=new Sequelize(
-    "zammatech",
-    "root",
-    "8126",
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
-        host:"localhost",
-        dialect:"mysql"
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 3306,
+        dialect: "mysql"
     }
 );
 
-module.exports=sequelize;
+module.exports = sequelize;
